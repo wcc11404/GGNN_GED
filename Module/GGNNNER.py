6eb3f2dc6de50610a1ff4b176262dd47978a8ca0
hsams <chengyu_3663@126.com>
2019-12-17 14:33:38 +0800

    GGNNNER

--- Module/GGNNNER.py
+++ Module/GGNNNER.py
@@ -52,7 +52,7 @@ class GGNNNER(nn.Module):
 
         out = self.wordembedding(batchinput)
         out = self.gnn(out, graph_in, graph_out)
-        out, _ = self.rnn(out, batchlength)    # B S E
+        # out, _ = self.rnn(out, batchlength)    # B S E
 
         # lm_input = out.view(-1, out.shape[1], 2, out.shape[2] // 2).permute(2, 0, 1, 3).contiguous()  # 分成双向的
         # lm_fw_input, lm_bw_input = lm_input[0], lm_input[1]
