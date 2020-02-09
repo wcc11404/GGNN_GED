ccc0c4a2ab7b0ac96392c3f8081879749dd4ee45
hsams <chengyu_3663@126.com>
2020-02-09 22:55:33 +0800

    GGNNNER

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 #python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
 
 head -n 1000000 data/process/pretrain.train > data/process/pretrain.train.1M
-python scripts/genGraph.py --mode 1 --input data/process/pretrain.train.1M --output data/process/pretrain.train.graph --process data/process.pretrain.train.ic --stanford data/stanford-corenlp-full-2018-10-05
-python scripts/genGraph.py --mode 1 --input data/process/pretrain.dev --output data/process/pretrain.dev.graph --process data/process.pretrain.dev.ic --stanford data/stanford-corenlp-full-2018-10-05
+head -n 20000 data/process/pretrain.dev > data/process/pretrain.dev.20K
+python scripts/genGraph.py --mode 1 --input data/process/pretrain.train.1M --output data/process/pretrain.train.graph --process data/process/pretrain.train.ic --stanford data/stanford-corenlp-full-2018-10-05
+python scripts/genGraph.py --mode 1 --input data/process/pretrain.dev.20K --output data/process/pretrain.dev.graph --process data/process/pretrain.dev.ic --stanford data/stanford-corenlp-full-2018-10-05
 
 # 预处理的预处理
 # tokenize_()
