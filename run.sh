80776863b103db1a78ebd6ca4be018d2cb9d87d5
hsams <chengyu_3663@126.com>
2020-02-09 21:51:35 +0800

    GGNNNER

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
 python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.train.txt --output data/process/pretrain.train --stanford data/stanford-corenlp-full-2018-10-05
+python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
 # 预处理的预处理
 # tokenize_()
 # generate_graph(mode=0)
