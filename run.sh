47c2b3f8ab944616a29c6f4a7cf8e0b7c24dea1c
hsams <chengyu_3663@126.com>
2020-02-09 22:46:36 +0800

    GGNNNER

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.train.txt --output data/process/pretrain.train --stanford data/stanford-corenlp-full-2018-10-05
-python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
+#python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.train.txt --output data/process/pretrain.train --stanford data/stanford-corenlp-full-2018-10-05
+#python scripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
 
-#python scripts/genGraph.py --mode 1 --input data/process/pretrain.train --output data/process/pretrain.train.graph --process data/process.pretrain.train.ic --stanford data/stanford-corenlp-full-2018-10-05
-#python scripts/genGraph.py --mode 1 --input data/process/pretrain.dev --output data/process/pretrain.dev.graph --process data/process.pretrain.dev.ic --stanford data/stanford-corenlp-full-2018-10-05
+head -n 1000000 data/process/pretrain.train > data/process/pretrain.train.1M
+python scripts/genGraph.py --mode 1 --input data/process/pretrain.train.1M --output data/process/pretrain.train.graph --process data/process.pretrain.train.ic --stanford data/stanford-corenlp-full-2018-10-05
+python scripts/genGraph.py --mode 1 --input data/process/pretrain.dev --output data/process/pretrain.dev.graph --process data/process.pretrain.dev.ic --stanford data/stanford-corenlp-full-2018-10-05
 
 # 预处理的预处理
 # tokenize_()
