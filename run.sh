21fe6288ff8c128c9470e448ae9c265ab8b1cd54
hsams <chengyu_3663@126.com>
2020-02-10 13:53:17 +0800

    GGNNNER

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@
 #python scripts/genGraph.py --mode 1 --input data/process/pretrain.train.1M --output data/process/pretrain.train.1M.graph --process data/process/pretrain.train.1M.ic --stanford data/stanford-corenlp-full-2018-10-05
 #python scripts/genGraph.py --mode 1 --input data/process/pretrain.dev.20K --output data/process/pretrain.dev.20K.graph --process data/process/pretrain.dev.20K.ic --stanford data/stanford-corenlp-full-2018-10-05
 
-python scripts/genVocab.py --mode 0 --input data/process/pretrain.train.1M.ic data/process/fce-public.train.preprocess.tsv data/process/fce-public.dev.preprocess.tsv --output data/prepare/wordvocab.pkl
+#data/process/pretrain.train.1M.ic
+python scripts/genVocab.py --mode 0 --input data/process/fce-public.train.preprocess.tsv data/process/fce-public.dev.preprocess.tsv --output data/prepare/wordvocab.pkl
 python scripts/genVocab.py --mode 1 --input data/process/pretrain.train.1M.ic data/process/fce-public.train.preprocess.tsv data/process/fce-public.dev.preprocess.tsv --output data/prepare/charvocab.pkl
 python scripts/genVocab.py --mode 2 --input data/process/pretrain.train.1M.graph data/process/train_graph.txt data/process/dev_graph.txt --output data/prepare/edgevocab.pkl
 # 预处理的预处理
