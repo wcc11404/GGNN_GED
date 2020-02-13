42b22dfa71e4bac90ea07f4f9b1b2fb8876d0d7e
hsams <chengyu_3663@126.com>
2020-02-13 19:58:01 +0800

    LM_GGNNNER

--- myscripts/utils.py
+++ myscripts/utils.py
@@ -23,11 +23,11 @@ def load_checkpoint(model, dir):
         load_checkpoint = torch.load(best_dir)  # 找一下最好的
         model_dict = model.state_dict()  # 获得当前模型的参数字典
         load_dict = {k: v for k, v in load_checkpoint.items() if k in model_dict}  # 找名字一样的加载权重
-        print(model.wordembedding.wordembedding.weight)
+        # print(model.wordembedding.wordembedding.weight)
         model.load_state_dict(load_dict)  # 加载权重
-        print()
-        print(model.wordembedding.wordembedding.weight)
-        exit()
+        # print()
+        # print(model.wordembedding.wordembedding.weight)
+        # exit()
     except:
         print("failed to load")
 
