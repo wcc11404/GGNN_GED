f20b3d4ed7512b55d8c4beca4555e2f0de96d7f4
hsams <chengyu_3663@126.com>
2020-02-14 16:15:44 +0800

    GGNNNER_attention

--- inference.sh
+++ inference.sh
@@ -1,3 +1,4 @@
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # 测试
 python $script_dir/main.py --gpu-id 4 --mode Test --arch GGNNNER --load-dir checkpoint/GGNN_att \
  --data-dir data/prepare/train.pkl
