f25b5ad7eac987bfe1da98f75ab8477f6d8fa230
hsams <chengyu_3663@126.com>
2020-02-14 12:36:41 +0800

    LM_GGNNNER

--- run.sh
+++ run.sh
@@ -51,4 +51,5 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 python $script_dir/main.py --gpu-id 0 --mode Train --arch GGNNNER --w2v-dir data/process/w2v_300d.txt \
  --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/GGNN_test \
  --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
- --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.0
+ --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.0 \
+ 2>&1 | tee -a checkpoint/training.log
