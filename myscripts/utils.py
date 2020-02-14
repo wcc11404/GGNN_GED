9463fba9530185584860f4a175cfbbbb5dde7d66
hsams <chengyu_3663@126.com>
2020-02-14 10:17:05 +0800

    LM_GGNNNER

--- myscripts/utils.py
+++ myscripts/utils.py
@@ -16,16 +16,17 @@ def load_checkpoint(model, dir):
         raise KeyError("checkpoint is not exist")
 
     # TODO 输入可以是具体的权重文件而不是文件夹
-    # try:
-    log = os.path.join(dir, "save.log")
-    best_dir = open(log, "r").readline().strip()
-    print("load checkpoint from " + best_dir)
-    load_checkpoint = torch.load(best_dir, map_location={'cuda:1':'cuda:0'})  # 找一下最好的
-    model_dict = model.state_dict()  # 获得当前模型的参数字典
-    load_dict = {k: v for k, v in load_checkpoint.items() if k in model_dict}  # 找名字一样的加载权重
-    model.load_state_dict(load_dict)  # 加载权重
-    # except:
-    #     print("failed to load")
+    try:
+        log = os.path.join(dir, "save.log")
+        best_dir = open(log, "r").readline().strip()
+        print("load checkpoint from " + best_dir)
+        # 之前save会存储gpu信息，所以可能会导致load cuda error（之前gpu被占）
+        load_checkpoint = torch.load(best_dir, map_location=lambda storage, loc: storage)
+        model_dict = model.state_dict()  # 获得当前模型的参数字典
+        load_dict = {k: v for k, v in load_checkpoint.items() if k in model_dict}  # 找名字一样的加载权重
+        model.load_state_dict(load_dict)  # 加载权重
+    except:
+        print("failed to load")
 
 def save_args(dic, dir):
     with open(dir, 'w') as f:
