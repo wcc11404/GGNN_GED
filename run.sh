d64d5450f83b4aeff8d7e3822f215f5374968bd8
hsams <chengyu_3663@126.com>
2020-02-15 15:50:40 +0800

    GGNNNER_attention

--- run.sh
+++ run.sh
@@ -42,7 +42,7 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # --max-epoch 12
 
 # fine-tune
-python $script_dir/main.py --gpu-id 1 --mode Train --arch GGNNNER \
+python $script_dir/main.py --gpu-id 1 3 4 5 --mode Train --arch GGNNNER \
  --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/GGNN_new --load-dir checkpoint/LM_GGNN_new \
  --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
  --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.1
