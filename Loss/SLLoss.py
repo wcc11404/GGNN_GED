4546266f8026ae8bc912c3b83777648045eddbd5
hsams <chengyu_3663@126.com>
2020-02-15 22:19:37 +0800

    multigpu

--- Loss/SLLoss.py
+++ Loss/SLLoss.py
@@ -11,13 +11,13 @@ class SLLoss(nn.Module):
         self.forwardLoss = nn.CrossEntropyLoss(ignore_index=-1, reduction="sum")
         self.bakwardLoss = nn.CrossEntropyLoss(ignore_index=-1, reduction="sum")
 
-    def forward(self, out, lm_fw_out, lm_bw_out, label, forwardlabel):
+    def forward(self, out, lm_fw_out, lm_bw_out, label):#, forwardlabel
         # output, (label, extra_label) = inputs
         print("out:"+str(len(out)))
         print("lm_fw_out:" + str(len(lm_fw_out)))
         print("lm_bw_out:" + str(len(lm_bw_out)))
         print("label:" + str(len(label)))
-        print("forwardlabel:" + str(len(forwardlabel)))
+        # print("forwardlabel:" + str(len(forwardlabel)))
 
         # out, (lm_fw_out, lm_bw_out) = output
         loss = self.Loss(out.view(-1, 2), label.view(-1))
