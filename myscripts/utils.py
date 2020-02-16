78a1994cf52d6d1b5da58f749db9ef57c5a23913
hsams <chengyu_3663@126.com>
2020-02-16 20:28:19 +0800

    multigpu

--- myscripts/utils.py
+++ myscripts/utils.py
@@ -29,8 +29,8 @@ def load_checkpoint(model, dir):
         model_dict = model.state_dict()  # 获得当前模型的参数字典
         load_dict = {k: v for k, v in load_checkpoint.items() if k in model_dict}  # 找名字一样的加载权重
         model.load_state_dict(load_dict)  # 加载权重
-    except:
-        print("failed to load")
+    except Exception:
+        print(Exception)
 
 def save_args(dic, dir):
     with open(dir, 'w') as f:
