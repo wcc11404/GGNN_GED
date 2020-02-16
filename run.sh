094cf6150db1705ab82752baa1083c08cf0079ee
hsams <chengyu_3663@126.com>
2020-02-16 11:09:34 +0800

    multigpu

--- run.sh
+++ run.sh
@@ -35,17 +35,17 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # --word-vocab-dir data/prepare/wordvocab.pkl --char-vocab-dir data/prepare/charvocab.pkl --edge-vocab-dir data/prepare/edgevocab.pkl --output data/prepare/train.pkl
 
 # 预训练
-python $script_dir/main.py --gpu-id 1 --mode Train --arch GGNNNER --criterion LMLoss \
- --char-embed-dim 0 --gnn-steps 2 --save-dir checkpoint/LM_GGNN_new_step2 \
- --w2v-dir data/process/w2v_300d.txt --data-dir data/prepare/pretrain.pkl \
- --optimizer adam --lr 1e-3 --evaluation loss --batch-size 64 --early-stop 5 \
- --max-epoch 12
+#python $script_dir/main.py --gpu-id 1 --mode Train --arch GGNNNER --criterion LMLoss \
+# --char-embed-dim 0 --gnn-steps 2 --save-dir checkpoint/LM_GGNN_new_step2 \
+# --w2v-dir data/process/w2v_300d.txt --data-dir data/prepare/pretrain.pkl \
+# --optimizer adam --lr 1e-3 --evaluation loss --batch-size 64 --early-stop 5 \
+# --max-epoch 12
 
 # fine-tune
-#python $script_dir/main.py --gpu-id 1 3 --mode Train --arch GGNNNER --criterion SLLoss \
-# --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/GGNN_new --load-dir checkpoint/LM_GGNN_new \
-# --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
-# --batch-size 16 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.1
+python $script_dir/main.py --gpu-id 3 --mode Train --arch GGNNNER --criterion SLLoss \
+ --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/GGNN_new --load-dir checkpoint/LM_GGNN_new \
+ --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
+ --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.1
 
 # 训练
 #python $script_dir/main.py --gpu-id 4 --mode Train --arch GGNNNER --w2v-dir data/process/w2v_300d.txt \
