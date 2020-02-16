c30cf15d0a9578ca01fc5083c3c796aa294c8abf
hsams <chengyu_3663@126.com>
2020-02-16 21:47:02 +0800

    multigpu

--- run.sh
+++ run.sh
@@ -11,14 +11,14 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #python $script_dir/myscripts/genGraph.py --mode 0 --input data/process/fce-public.test.preprocess.tsv --output data/process/test_graph.txt --stanford data/stanford-corenlp-full-2018-10-05
 
 # tokenize无标签数据
-python $script_dir/myscripts/mytokenize.py --mode 1 --input data/orign_data/1b.train.txt --output data/process/pretrain.train --stanford data/stanford-corenlp-full-2018-10-05
-python $script_dir/myscripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
+#python $script_dir/myscripts/mytokenize.py --mode 1 --input data/orign_data/1b.train.txt --output data/process/pretrain.train --stanford data/stanford-corenlp-full-2018-10-05
+#python $script_dir/myscripts/mytokenize.py --mode 1 --input data/orign_data/1b.dev.txt --output data/process/pretrain.dev --stanford data/stanford-corenlp-full-2018-10-05
 
 # 生成无标签数据的依赖图（部分）
-#head -n 1000000 data/process/pretrain.train > data/process/pretrain.train.1M
-#head -n 20000 data/process/pretrain.dev > data/process/pretrain.dev.20K
-#python $script_dir/myscripts/genGraph.py --mode 1 --input data/process/pretrain.train.1M --output data/process/pretrain.train.1M.graph --process data/process/pretrain.train.1M.ic --stanford data/stanford-corenlp-full-2018-10-05
-#python $script_dir/myscripts/genGraph.py --mode 1 --input data/process/pretrain.dev.20K --output data/process/pretrain.dev.20K.graph --process data/process/pretrain.dev.20K.ic --stanford data/stanford-corenlp-full-2018-10-05
+head -n 5000000 data/process/pretrain.train > data/process/pretrain.train.5M
+head -n 100000 data/process/pretrain.dev > data/process/pretrain.dev.100K
+python $script_dir/myscripts/genGraph.py --mode 1 --input data/process/pretrain.train.5M --output data/process/pretrain.train.5M.graph --process data/process/pretrain.train.5M.ic --stanford data/stanford-corenlp-full-2018-10-05
+python $script_dir/myscripts/genGraph.py --mode 1 --input data/process/pretrain.dev.100K --output data/process/pretrain.dev.100K.graph --process data/process/pretrain.dev.100K.ic --stanford data/stanford-corenlp-full-2018-10-05
 
 # 生成word、char以及edge词典
 #python $script_dir/myscripts/genVocab.py --mode 0 --input data/process/fce-public.train.preprocess.tsv data/process/fce-public.dev.preprocess.tsv --output data/prepare/wordvocab.pkl --mergeinput data/process/pretrain.train.1M.ic --mergemaxnum 50000
