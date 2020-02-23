9a7ae236d2fc19fffd5c297cc8cdd12832c04e05
hsams <chengyu_3663@126.com>
2020-02-23 23:03:54 +0800

    GAN

--- run.sh
+++ run.sh
@@ -47,7 +47,7 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
 # --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.02
 
-# 训练 adadelta
+# 训练
 python -u $script_dir/main.py --gpu-id 0 --mode Train --arch GANNER --criterion BaseLoss \
  --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/GAN_step3 --w2v-dir data/process/w2v_300d.txt \
  --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
