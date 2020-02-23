bcef88739b9b0e89a11ced2f1adaec526e435729
hsams <chengyu_3663@126.com>
2020-02-23 14:35:17 +0800

    GAN

--- Module/GraphAttention.py
+++ Module/GraphAttention.py
@@ -11,12 +11,12 @@ class GraphAttentionTemplate(nn.Module):
         self.n_head = n_head
         assert self.input_dim % self.n_head == 0
 
-        # self.weight_a = nn.Conv1d(self.input_dim, self.input_dim // self.n_head, 1, bias=False)
-        # self.weight_b = nn.Conv1d(self.input_dim // self.n_head, 1, 1)
-        # self.weight_c = nn.Conv1d(self.input_dim // self.n_head, 1, 1)
-        self.weight_a = LinearTemplate(self.input_dim, self.input_dim, use_bias=False)
-        self.weight_b = LinearTemplate(self.input_dim, self.input_dim)
-        self.weight_c = LinearTemplate(self.input_dim, self.input_dim)
+        self.weight_a = nn.Conv1d(self.input_dim, self.input_dim // self.n_head, 1, bias=False)
+        self.weight_b = nn.Conv1d(self.input_dim // self.n_head, 1, 1)
+        self.weight_c = nn.Conv1d(self.input_dim // self.n_head, 1, 1)
+        # self.weight_a = LinearTemplate(self.input_dim, self.input_dim, use_bias=False)
+        # self.weight_b = LinearTemplate(self.input_dim, self.input_dim)
+        # self.weight_c = LinearTemplate(self.input_dim, self.input_dim)
         self.bias = nn.Parameter(torch.FloatTensor(self.input_dim // self.n_head))
 
         self.dropout = nn.Dropout(dropout)
@@ -31,55 +31,48 @@ class GraphAttentionTemplate(nn.Module):
             if 'bias' in name:
                 nn.init.constant_(param, 0.01)
             elif 'weight' in name:
-                nn.init.xavier_uniform_(param)
+                nn.init.kaiming_normal_(param, mode='fan_out')
         for name, param in self.weight_b.named_parameters():
             if 'bias' in name:
                 nn.init.constant_(param, 0.01)
             elif 'weight' in name:
-                nn.init.xavier_uniform_(param)
+                nn.init.kaiming_normal_(param, mode='fan_out')
         for name, param in self.weight_c.named_parameters():
             if 'bias' in name:
                 nn.init.constant_(param, 0.01)
             elif 'weight' in name:
-                nn.init.xavier_uniform_(param)
-        # for name, param in self.weight_a.named_parameters():
-        #     if 'bias' in name:
-        #         nn.init.constant_(param, 0.01)
-        #     elif 'weight' in name:
-        #         nn.init.kaiming_normal_(param, mode='fan_out')
-        # for name, param in self.weight_b.named_parameters():
-        #     if 'bias' in name:
-        #         nn.init.constant_(param, 0.01)
-        #     elif 'weight' in name:
-        #         nn.init.kaiming_normal_(param, mode='fan_out')
-        # for name, param in self.weight_c.named_parameters():
-        #     if 'bias' in name:
-        #         nn.init.constant_(param, 0.01)
-        #     elif 'weight' in name:
-        #         nn.init.kaiming_normal_(param, mode='fan_out')
+                nn.init.kaiming_normal_(param, mode='fan_out')
         nn.init.constant_(self.bias, 0.01)
 
     def head_attention(self, input):
         # Aggregater
         out = self.dropout(input) # B * S * E
 
-        # out = out.permute(0, 2, 1).contiguous() # B * E * S
+        out = out.permute(0, 2, 1).contiguous() # B * E * S
         out = self.weight_a(out) # conv = [E, E//n_head, 1] => B * (E//n_head) * S
         temp1 = self.weight_b(out) # conv = [E//n_head, 1, 1] => B * 1 * S
         temp2 = self.weight_c(out) # conv = [E//n_head, 1, 1] => B * 1 * S
-        # temp1 = temp1.permute(0, 2, 1).contiguous() + temp2 # B * S * 1 + B * 1 * S => B * S * S
-        temp1 = torch.bmm(temp1, temp2.permute(0, 2, 1).contiguous()) # B * S * 1 + B * 1 * S => B * S * S
+        temp1 = temp1.permute(0, 2, 1).contiguous() + temp2 # B * S * 1 + B * 1 * S => B * S * S
+        # temp1 = torch.bmm(temp1, temp2.permute(0, 2, 1).contiguous()) # B * S * 1 + B * 1 * S => B * S * S
         coefs = nn.functional.softmax(nn.functional.leaky_relu(temp1, negative_slope=0.2), dim=-1)  # paper B * S * S
+        print("coefs")
+        print(coefs)
+        print()
         # coefs = nn.functional.softmax(torch.tanh(temp1), dim=-1)  # paper B * S * S
-        # out = out.permute(0, 2, 1).contiguous()  # B * S * (E//n_head)
-
+        out = out.permute(0, 2, 1).contiguous()  # B * S * (E//n_head)
+        print("out")
+        print(out)
+        print()
         coefs = self.dropout(coefs)
         out = self.dropout(out)
 
         # Updater
         re = torch.matmul(coefs, out) # B * S * (E//n_head)
+        print("re")
+        print(re)
+        print()
         re = re + self.bias # B * S * (E//n_head)
-
+        exit()
         if self.residual:
             re = re + out
         return re
