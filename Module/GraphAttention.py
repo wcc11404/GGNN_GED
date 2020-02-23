4917bcb5f1d85479e4432827956de82e330f16c7
hsams <chengyu_3663@126.com>
2020-02-23 15:08:58 +0800

    GAN

--- Module/GraphAttention.py
+++ Module/GraphAttention.py
@@ -54,11 +54,12 @@ class GraphAttentionTemplate(nn.Module):
         temp2 = self.weight_c(out) # conv = [E//n_head, 1, 1] => B * 1 * S
         temp1 = temp1.permute(0, 2, 1).contiguous() + temp2 # B * S * 1 + B * 1 * S => B * S * S
         # temp1 = torch.bmm(temp1, temp2.permute(0, 2, 1).contiguous()) # B * S * 1 + B * 1 * S => B * S * S
-        coefs = nn.functional.softmax(nn.functional.leaky_relu(temp1, negative_slope=0.2), dim=-1)  # paper B * S * S
+        # coefs = nn.functional.softmax(nn.functional.leaky_relu(temp1, negative_slope=0.2), dim=-1)  # paper B * S * S
+        coefs = nn.functional.softmax(torch.tanh(temp1), dim=-1)  # paper B * S * S
         print("coefs")
         print(coefs)
         print()
-        # coefs = nn.functional.softmax(torch.tanh(temp1), dim=-1)  # paper B * S * S
+
         out = out.permute(0, 2, 1).contiguous()  # B * S * (E//n_head)
         print("out")
         print(out)
