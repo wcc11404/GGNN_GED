824f27719c2468752dccb659a6943f0a7573fc08
hsams <chengyu_3663@126.com>
2020-02-28 22:21:02 +0800

    GAN

--- run.sh
+++ run.sh
@@ -55,7 +55,7 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # --data-dir data/prepare/train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
 # --batch-size 32 --early-stop 8 --max-epoch 50 --lm-cost-weight 0.02
 
-# 训练
+# 训练 
 python -u $script_dir/main.py --gpu-id 0 1 2 3 --mode Train --arch GGNNNER --criterion SLLoss \
  --char-embed-dim 0 --gnn-steps 1 --save-dir checkpoint/sum_GGNNNER --w2v-dir data/process/w2v_300d.txt \
  --data-dir data/prepare/pretrain_corrupt_train.pkl --optimizer adadelta --lr 1 --evaluation f0.5 \
