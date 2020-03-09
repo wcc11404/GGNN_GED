6655947f60cb17446c898b14aeeb0c3da2f972d2
hsams <chengyu_3663@126.com>
2020-03-09 17:55:50 +0800

    GAN

--- myscripts/genGraph.py
+++ myscripts/genGraph.py
@@ -116,16 +116,16 @@ def generate_graph(args, addcontextedge=False):
                     for i in range(i2 - i1):
                         newlabel[j1 + i] = label[i1 + i]
                 elif tag == "replace":
-                    try:
-                        assert j2 - j1 - i2 + i1 >= 0
+                    if j2 - j1 - i2 + i1 >= 0:
                         for i in range(i2 - i1):
                             newlabel[j1 + i] = label[i1 + i]
                         for i in range(j2 - j1 - i2 + i1):
                             newlabel[i2 - i1 + j1 + i] = label[i2 - 1]
-                    except:
-                        print(temp)
-                        print(sen)
-                        print(tag, i1, i2, j1, j2)
+                    else:
+                        for i in range(j2 - j1):
+                            newlabel[j1 + i] = label[i1 + i]
+                elif tag == "delete":
+                    pass
                 else:
                     print(temp)
                     print(sen)
