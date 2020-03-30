58613af957a471aff2d5821e1fb1ae63c9f59ee8
hsams <chengyu_3663@126.com>
2020-03-30 18:37:08 +0800

    GAN

--- Module/BaseNER.py
+++ Module/BaseNER.py
@@ -6,20 +6,18 @@ class BaseNER(nn.Module):
     def __init__(self, args):
         super(BaseNER, self).__init__()
         self.wordembedding = EmbeddingTemplate(args.word_vocabulary_size, args.word_embed_dim, args.embed_drop)
-        self.rnn = RnnTemplate(args.rnn_type, args.batch_size, args.word_embed_dim+args.char_embed_dim, args.word_embed_dim, args.rnn_drop,
-                               bidirectional=args.rnn_bidirectional)
+        self.rnn = RnnTemplate(args.rnn_type, args.batch_size, args.word_embed_dim + args.char_embed_dim,
+                               args.word_embed_dim, args.rnn_drop, bidirectional=args.rnn_bidirectional)
 
         if args.char_embed_dim is not None and args.char_embed_dim > 0:
             self.charembedding = EmbeddingTemplate(args.char_vocabulary_size, args.char_embed_dim, args.embed_drop)
             self.charrnn = RnnTemplate(args.rnn_type, args.batch_size, args.char_embed_dim, args.char_embed_dim,
                                        args.rnn_drop)
-            self.hiddenlinear = LinearTemplate(args.word_embed_dim, args.hidden_dim,
-                                               activation="tanh", dropout=args.linear_drop)
         else:
             self.charembedding = None
-            self.hiddenlinear = LinearTemplate(args.word_embed_dim, args.hidden_dim, activation="tanh",
-                                               dropout=args.linear_drop)
 
+        self.hiddenlinear = LinearTemplate(args.word_embed_dim, args.hidden_dim, activation="tanh",
+                                               dropout=args.linear_drop)
         self.classification = LinearTemplate(args.hidden_dim, 2, activation=None)
 
         self.load_embedding(args)
@@ -40,8 +38,7 @@ class BaseNER(nn.Module):
             charout = self.charembedding(batchinput_char)
             _, charout = self.charrnn(charout, batchlength_char, ischar=True) # B S 2 E//2
             charout = charout.view(charout.shape[0], charout.shape[1], -1)
-
-        out = torch.cat((out, charout), 2)
+            out = torch.cat((out, charout), 2)
 
         out, _ = self.rnn(out, batchlength)    # B S E
 
